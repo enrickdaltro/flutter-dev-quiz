f6ed7c8448cffd9ee9bb00949f5c4bd3a036a808
Enrick <enrickdaltro@gmail.com>
2021-04-23 14:56:36 -0300

    feat: home screen and appBar widget

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,3 +1,4 @@
+import 'package:dev_quiz/home/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:dev_quiz/splash/splash_page.dart';
 
@@ -5,8 +6,6 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "DevQuiz",
-        home: SplashPage());
+        debugShowCheckedModeBanner: false, title: "DevQuiz", home: HomePage());
   }
 }
---
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -0,0 +1,46 @@
+import 'package:dev_quiz/core/core.dart';
+import 'package:dev_quiz/home/widgets/chart/chart_widget.dart';
+import 'package:flutter/material.dart';
+
+class ScoreCardWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+      child: Container(
+        height: 136,
+        decoration: BoxDecoration(
+            color: AppColors.white, borderRadius: BorderRadius.circular(15)),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 1,
+              child: ChartWidget(),
+            ),
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 24.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Vamos começar?',
+                      style: AppTextStyles.heading,
+                    ),
+                    Text(
+                      'Complete os desafios e avançe em conhecimento',
+                      style: AppTextStyles.body,
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
